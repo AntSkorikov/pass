f134aa53290ea987fcf424baa3b6a466ee4087e3
Егор <altego777@gmail.com>
2016-06-15 23:02:45 +0300

    Готово, проверяйте :)

--- script2.js
+++ script2.js
@@ -1,7 +1,22 @@
-var box = documentQuerySelector("");
-function run () {
-	if (true) {
+var passw = document.getElementById('CheckPass');
+var wrong = document.getElementById('WrongPass');
+var ok = document.getElementById('OkPass');
 
-	}
-	else {}
+function checkPassword (e) {
+    e.preventDefault();
+
+    var password = passw.value;
+    if (password != '1') {
+        passw.value = '';
+        wrong.style.display = "block";
+
+        setTimeout(function () {
+            wrong.style.display = "none";
+        }, 500);
+        return false;
+    }
+
+    ok.style.display = "block";
+    passw.style.display = "none";
+    return false;
 }
