6ff73be4f8382f1f0a09ab314fb7a723f53b495c
Anton <tsh5840@gmail.com>
2016-06-16 22:00:11 +0300

    взял на себя смелость немного подкорректировать скрипт

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
-var passw = document.getElementById('CheckPass');
-var wrong = document.getElementById('WrongPass');
-var ok = document.getElementById('OkPass');
+var passw = document.querySelector('#CheckPass'),
+	wrong = document.querySelector('#WrongPass'),
+	ok = document.querySelector('#OkPass');
 
 function checkPassword (e) {
     e.preventDefault();
@@ -8,14 +8,16 @@ function checkPassword (e) {
     var password = passw.value;
     if (password != '1') {
         passw.value = '';
+        passw.style.display = "none";
         wrong.style.display = "block";
-
         setTimeout(function () {
             wrong.style.display = "none";
-        }, 500);
+        }, 300);
+        setTimeout(function () {
+            passw.style.display = "block";
+        }, 300);
         return false;
     }
-
     ok.style.display = "block";
     passw.style.display = "none";
     return false;
